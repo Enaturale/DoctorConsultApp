2bcafeceabe17b0b2f204cc064efb8a47cd7dabe
Enaturale <enaturaleb@gmail.com>
2022-08-08 17:45:17 +0100

    home page opacity and image background done

--- src/screens/Homescreen/HomeScreen.js
+++ src/screens/Homescreen/HomeScreen.js
@@ -1,36 +1,70 @@
 import React from 'react';
 
-import {View, Text, ImageBackground, StyleSheet, Pressable} from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, Pressable } from 'react-native';
 
-const sourceImage = "https://pixabay.com/photos/stethoscope-hospital-doctor-health-840125/"
+
 
 const HomeScreen = () => {
-    return(
-        <ImageBackground source={sourceImage} style={styles.background}>
-            <View>
-            <Text>
-                Consult from the comfort of your home
-            </Text>
+    return (
+        <View style={{ flex: 1 }}>
 
-            <Pressable>
-                <Text>Log In</Text>
-            </Pressable>
+            <ImageBackground source={require('../../../assets/images/sethso.jpg')} style={styles.background} resizeMode="cover">
+                <View style={styles.container}>
+                    <View style={{marginHorizontal: 20,}}>
+                        <Text style={styles.title}>
+                            Consult from the comfort of your home
+                        </Text>
+                    </View>
 
-            <Pressable>
-                <Text>Sign Up</Text>
-            </Pressable>
-            </View>           
-        </ImageBackground>
+
+                    <Pressable style={styles.loginBtn}>
+                        <Text>Log In</Text>
+                    </Pressable>
+
+                    <Pressable style={styles.signupBtn}>
+                        <Text>Sign Up</Text>
+                    </Pressable>
+                </View>
+            </ImageBackground>
+        </View>
     );
 };
 
-const styles=StyleSheet.create({
-    background:{
+const styles = StyleSheet.create({
+    background: {
         flex: 1,
     },
-    container:{
-        
+    container: {
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 500,
+        marginTop: 130,
+        backgroundColor: 'white',
+        borderRadius: 15,
+        opacity: 0.5,
+    },
+    title: {
+        fontSize: 25,
+        fontFamily: 'Nunito-Black',
+        color: 'black',
+    },
+    loginBtn: {
+        height: 50,
+        width: 150,
+        borderRadius: 15,
+        borderColor: 'black',
+        borderWidth: 1,
+
+    },
+    signupBtn: {
+        height: 50,
+        width: 150,
+        borderRadius: 15,
+        borderColor: 'black',
+        borderWidth: 1,
     }
+
 
 })
 
